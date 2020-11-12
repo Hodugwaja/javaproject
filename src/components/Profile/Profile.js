2a9291ef57935437af6fe9d5ccf9ed3a073f3648
HoduGwaja <honeybutter9250@gmail.com>
2020-11-12 19:12:21 +0900

    [commit | 2020.11.12 | added profile page]

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,7 +7,7 @@ const Profile = () => {
         <div className = "background-profile">
             <div className = "background-white-profile">
                 <div className = "form-profile">
-                    <div className = "text-profile">Email <input className = "input-profile"/></div>
+                    <div className = "text-profile">Email <input className = "input-profile" placeholder = " nickname@gmail.com"/></div>
                     <br/><br/>
                     <div className = "text-profile">NickName <input className = "input-profile"/></div>
                     <br/><br/>
@@ -15,8 +15,8 @@ const Profile = () => {
                     <br/><br/>
                     <div className = "text-profile">Confrim Password <input className = "input-profile"/></div>
                     <br/><br/>
-                    <Link to = "/main"><button className = "button-profile">Cancel</button></Link>
-                    <Link to = "/main"><button className = "button-profile">Save</button></Link>
+                    <Link to = "/profile"><button className = "cancel-profile">Cancel</button></Link>
+                    <Link to = "/main"><button className = "save-profile">Save</button></Link>
                     
                 </div>
             </div>
