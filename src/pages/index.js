7d3ba5f473e523ebfc827ab4960b3062920ea61d
HoduGwaja <honeybutter9250@gmail.com>
2020-11-16 15:14:40 +0900

    [commit | 2020.11.16 | added setting project page, changed all pages]

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export {default as Main} from './Main';
 export {default as signUp} from './signUp';
 export {default as Forgot} from './Forgot';
 export {default as Profile} from './Profile';
+export {default as setProject} from './setProject';
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import {Main, signUp, Forgot, Profile} from './../pages/index'
+import {Main, signUp, Forgot, Profile, setProject} from './../pages/index'
 
 const App = () => {
     return(
         <div>
-            <Route path="/main" component={Main} />
+            <Route exact path="/" component={Main} />
             <Route path="/Join" component={signUp} />
             <Route path="/Forgot" component={Forgot} />
             <Route path = '/profile' component = {Profile}/>
-            
+            <Route path = "/setProject" component = {setProject}/>
         </div>
     )
 };
